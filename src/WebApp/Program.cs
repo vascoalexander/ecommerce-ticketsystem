0ea807c35446b707ad0630eedea39fdfac3ba618
vascoalexander <vascoalexander@gmail.com>
2025-06-24 10:46:23 +0200

    Connect DB, add seeding method & automatic migrations

--- src/WebApp/Program.cs
+++ src/WebApp/Program.cs
@@ -1,9 +1,22 @@
+using WebApp.Data;
+using Microsoft.AspNetCore;
+using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
+
 var app = builder.Build();
+
+using var scope = app.Services.CreateScope();
+var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+db.Database.Migrate();
+DbInitializer.SeedDb(db);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
