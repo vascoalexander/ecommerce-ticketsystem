fd909d4df4f742a292d697fc695eda639bed2084
Spazapy <n.marcel.1994@gmx.de>
2025-06-24 14:03:46 +0200

    DefaultPage done

--- src/WebApp/Controllers/HomeController.cs
+++ src/WebApp/Controllers/HomeController.cs
@@ -13,15 +13,11 @@ public class HomeController : Controller
         _logger = logger;
     }
 
-    public IActionResult Index()
+    public IActionResult DefaultPage()
     {
         return View();
     }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+    
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
