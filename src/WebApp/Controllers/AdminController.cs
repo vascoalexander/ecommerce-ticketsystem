fba22d2b6ee4dc7f77ff013dee31f92b4ba827b9
Naceus <naco1606@gmail.com>
2025-06-25 14:46:47 +0200

    View für AdminBereich

---
+++ src/WebApp/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult AdminPage()
+        {
+            return View();
+        }
+    }
+}
