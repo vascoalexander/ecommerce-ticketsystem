0c67f7ff9df5d8cf0f56a5d88ce32bc53e3be1b9
vascoalexander <vascoalexander@gmail.com>
2025-06-25 15:34:07 +0200

    add project and ticket models + seeding

--- src/WebApp/Data/DbInitializer.cs
+++ src/WebApp/Data/DbInitializer.cs
@@ -1,9 +1,51 @@
+using WebApp.Models;
+
 namespace WebApp.Data;
 
 public static class DbInitializer
 {
-    public static void SeedDb(AppDbContext context)
+    public static void SeedDb(IApplicationBuilder app)
     {
-        context.SaveChanges();
+        var context = app
+            .ApplicationServices
+            .CreateScope()
+            .ServiceProvider
+            .GetRequiredService<AppDbContext>();
+
+        if (!context.Projects.Any())
+        {
+            var project1 = new ProjectModel
+            {
+                Title = "Project 1",
+                Description = "Project 1 description",
+                StartDate = new DateTime(2025, 6, 22,0,0,0,DateTimeKind.Utc),
+                EndDate = new DateTime(2026,6,10,0,0,0,DateTimeKind.Utc),
+            };
+            context.Projects.AddRange(project1);
+            context.SaveChanges();
+        }
+
+        if (!context.Tickets.Any())
+        {
+            var project1 = context.Projects.FirstOrDefault(p => p.Title == "Project 1");
+            var creatorUser = context.Users.FirstOrDefault(u => u.UserName == "tester");
+
+            if (project1 is null || creatorUser is null)
+                throw new Exception("Seed-Daten fehlen: Projekt 'Project 1' oder User 'tester' nicht gefunden.");
+
+            var ticket1 = new TicketModel
+            {
+                Title = "Ticket 1",
+                Description = "Ticket 1 description",
+                CreatedAt = new DateTime(2025, 6, 23,0,0,0, DateTimeKind.Utc),
+                AssignedAt = new DateTime(2026, 6, 23,0,0,0, DateTimeKind.Utc),
+                ProjectId = project1.Id,
+                Project = project1,
+                CreatorUser = creatorUser,
+                CreatorUserId = creatorUser.Id,
+            };
+            context.Tickets.AddRange(ticket1);
+            context.SaveChanges();
+        }
     }
 }
---
+++ src/WebApp/Models/ProjectModel.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models;
+
+public class ProjectModel
+{
+    public int Id { get; set; }
+    [StringLength(20, ErrorMessage = "Der Titel darf maximal 20 Zeichen lang sein.")]
+    public required string Title { get; set; }
+    [StringLength(400, ErrorMessage = "Die Beschreibung darf maximal 400 Zeichen lang sein.")]
+    public required string Description { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public virtual ICollection<TicketModel>? Tickets { get; set; }
+}
