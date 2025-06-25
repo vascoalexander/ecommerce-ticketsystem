2b79e36e4aa2f593c531ceb05ad1f04c8cb55c6d
vascoalexander <vascoalexander@gmail.com>
2025-06-25 10:51:04 +0200

    add Login functionality

--- src/WebApp/Controllers/HomeController.cs
+++ src/WebApp/Controllers/HomeController.cs
@@ -20,11 +20,6 @@ public class HomeController : Controller
 
     public IActionResult Dashboard() { return View(); }
 
-    public IActionResult Login() { return  View(); }
-    public IActionResult Privacy()
-    {
-        return View();
-    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
