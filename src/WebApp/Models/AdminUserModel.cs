ec682babc423f9571695f009523940080828e9c2
Naceus <161336167+Naceus@users.noreply.github.com>
2025-06-26 15:11:17 +0200

    feat: AdminUserModel erstellt

---
+++ src/WebApp/Models/AdminUserModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class AdminUserModel
+    {
+        public string? Id { get; set; }
+        public string? UserName { get; set; }
+        [Required(ErrorMessage ="Passwort ist erforderlich.")]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+        public IList<SelectListItem> Roles { get; set; } = new List<SelectListItem>();
+        [Required(ErrorMessage = "Rolle muss zugewiesen werden.")]
+        public string? Role { get; set; }
+
+    }
+}
