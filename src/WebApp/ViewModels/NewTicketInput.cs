1f57f44ca84326a564afd2b85022cb00ebc9fcab
Spazapy <121809787+Spazapy@users.noreply.github.com>
2025-06-30 11:45:36 +0200

    add: Close/reopen and Status change method for tickets

--- src/WebApp/ViewModels/NewTicketInput.cs
+++ src/WebApp/ViewModels/NewTicketInput.cs
@@ -6,7 +6,7 @@ public class NewTicketInputModel
     public string Title { get; set; } = "";
     public string Description { get; set; } = "";
 
-    public string AssignedUserId { get; set; } = "";
+    public string? AssignedUserId { get; set; } = "";
 
     public int ProjectId { get; set; }
     
