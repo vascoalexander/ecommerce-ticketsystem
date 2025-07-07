f715027f74534c259131c93deaeff5c6efa6802d
vascoalexander <vascoalexander@gmail.com>
2025-07-07 13:37:58 +0200

    feat: Migration for Message Deletion

---
+++ src/WebApp/Migrations/20250707090959_AddFlagsToMessages.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFlagsToMessages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeletedReceiver",
+                table: "Messages",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeletedSender",
+                table: "Messages",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeletedReceiver",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeletedSender",
+                table: "Messages");
+        }
+    }
+}
