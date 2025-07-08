b76c31a57b0d3130b81200500675a8cc072475ff
vascoalexander <vascoalexander@gmail.com>
2025-07-08 10:13:20 +0200

    refactor: add relevant files for service refactoring

---
+++ src/WebApp/Repositories/IRepository.cs
@@ -0,0 +1,10 @@
+namespace WebApp.Repositories;
+
+public interface IRepository<T>
+{
+    T GetById(int id);
+    IEnumerable<T> GetAll();
+    void Add(T entity);
+    void Update(T entity);
+    void Delete(int id);
+}
---
+++ src/WebApp/Services/AccountService.cs
@@ -0,0 +1,6 @@
+namespace WebApp.Services;
+
+public class AccountService
+{
+    
+}
